d52094b4f6844fdf7ab1cea9c6b57d13a4164b23
asterein <88514774+asterein@users.noreply.github.com>
2024-04-15 16:22:05 -0400

    consolidate dragonEgg and add html color names list

--- src/Dragon.elm
+++ src/Dragon.elm
@@ -51,12 +51,12 @@ type alias Point =
 
 init : () -> (Model, Cmd Msg)
 init _ =
-    ( dragonEgg 15 (Model [] 0 0 0 0 0 0 0 0)
+    ( dragonEgg 15
     , Cmd.none
     )
 
-dragonEgg : Int -> Model -> Model
-dragonEgg i model =
+dragonEgg : Int -> Model
+dragonEgg i =
   let
       p = [ Point i i, Point 0 i ]
       h = i * 2
@@ -66,6 +66,7 @@ dragonEgg i model =
       mnX = 0
       mxY = i
       mnY = 0
+      model = Model [] 0 0 0 0 0 0 0 0
   in
     { model | points = p
             , height = h
@@ -76,6 +77,150 @@ dragonEgg i model =
             , maxY = mxY
             , minY = mnY
             }
+
+-- list of html color names
+colors : List String
+colors =  [ "AliceBlue"
+          , "AntiqueWhite"
+          , "Aqua"
+          , "Aquamarine"
+          , "Azure"
+          , "Beige"
+          , "Bisque"
+          , "Black"
+          , "BlanchedAlmond"
+          , "Blue"
+          , "BlueViolet"
+          , "Brown"
+          , "Burlywood"
+          , "CadetBlue"
+          , "Chartreuse"
+          , "Chocolate"
+          , "Coral"
+          , "CornflowerBlue"
+          , "Cornsilk"
+          , "Crimson"
+          , "Cyan"
+          , "DarkBlue"
+          , "DarkCyan"
+          , "DarkGoldenrod"
+          , "DarkGray"
+          , "DarkGreen"
+          , "DarkKhaki"
+          , "DarkMagenta"
+          , "DarkOliveGreen"
+          , "DarkOrange"
+          , "DarkOrchid"
+          , "DarkRed"
+          , "DarkSalmon"
+          , "DarkSeaGreen"
+          , "DarkSlateBlue"
+          , "DarkSlateGray"
+          , "DarkTurquoise"
+          , "DarkViolet"
+          , "DeepPink"
+          , "DeepSkyBlue"
+          , "DimGray"
+          , "DodgerBlue"
+          , "Firebrick"
+          , "FloralWhite"
+          , "ForestGreen"
+          , "Fuchsia"
+          , "Gainsboro"
+          , "GhostWhite"
+          , "Gold"
+          , "Goldenrod"
+          , "Gray"
+          , "Green"
+          , "GreenYellow"
+          , "Honeydew"
+          , "HotPink"
+          , "IndianRed"
+          , "Indigo"
+          , "Ivory"
+          , "Khaki"
+          , "Lavender"
+          , "LavenderBlush"
+          , "LawnGreen"
+          , "LemonChiffon"
+          , "LightBlue"
+          , "LightCoral"
+          , "LightCyan"
+          , "LightGoldenrodYellow"
+          , "LightGray"
+          , "LightGreen"
+          , "LightPink"
+          , "LightSalmon"
+          , "LightSeaGreen"
+          , "LightSkyBlue"
+          , "LightSlateGray"
+          , "LightSteelBlue"
+          , "LightYellow"
+          , "Lime"
+          , "LimeGreen"
+          , "Linen"
+          , "Magenta"
+          , "Maroon"
+          , "MediumAquamarine"
+          , "MediumBlue"
+          , "MediumOrchid"
+          , "MediumPurple"
+          , "MediumSeaGreen"
+          , "MediumSlateBlue"
+          , "MediumSpringGreen"
+          , "MediumTurquoise"
+          , "MediumVioletRed"
+          , "MidnightBlue"
+          , "MintCream"
+          , "MistyRose"
+          , "Moccasin"
+          , "NavajoWhite"
+          , "Navy"
+          , "OldLace"
+          , "Olive"
+          , "OliveDrab"
+          , "Orange"
+          , "OrangeRed"
+          , "Orchid"
+          , "PaleGoldenrod"
+          , "PaleGreen"
+          , "PaleTurquoise"
+          , "PaleVioletRed"
+          , "PapayaWhip"
+          , "PeachPuff"
+          , "Peru"
+          , "Pink"
+          , "Plum"
+          , "PowderBlue"
+          , "Purple"
+          , "Red"
+          , "RosyBrown"
+          , "RoyalBlue"
+          , "SaddleBrown"
+          , "Salmon"
+          , "SandyBrown"
+          , "SeaGreen"
+          , "Seashell"
+          , "Sienna"
+          , "Silver"
+          , "SkyBlue"
+          , "SlateBlue"
+          , "SlateGray"
+          , "Snow"
+          , "SpringGreen"
+          , "SteelBlue"
+          , "Tan"
+          , "Teal"
+          , "Thistle"
+          , "Tomato"
+          , "Turquoise"
+          , "Violet"
+          , "Wheat"
+          , "White"
+          , "WhiteSmoke"
+          , "Yellow"
+          , "YellowGreen"
+          ]
 
 -- UPDATE
 
@@ -88,7 +233,7 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Reset ->
-      ( dragonEgg 15 (Model [] 0 0 0 0 0 0 0 0)
+      ( dragonEgg 15
       , Cmd.none
       )
     Render ->
